b0fb5662ec38bbf02e7a2f0079e7602b2a89e5de
daniel <daniel@localhost>
2021-07-09 12:26:13 +0200

    remove unneeded import

--- docdl/plugins/vodafonekabel_de.py
+++ docdl/plugins/vodafonekabel_de.py
@@ -2,7 +2,6 @@
 
 import docdl
 import itertools
-import selenium.common.exceptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
