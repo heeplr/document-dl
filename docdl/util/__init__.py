cd4e01c4bd5c1aa946223399138133b934f20af5
daniel <d@upquark>
2022-01-10 00:50:33 +0100

    parse_decimal() masseur

--- docdl/util/__init__.py
+++ docdl/util/__init__.py
@@ -8,6 +8,12 @@ import os
 from .dateparser import parse as parse_date  # noqa: F401 (import as shortcut)
 
 
+def parse_decimal(decimal):
+    """massage string with decimal number"""
+    decimal = decimal.split()[0]
+    decimal = decimal.replace(',','.')
+    return decimal
+
 def show_image(filename, name="image"):
     """attempt to show image"""
     # always print image filename
