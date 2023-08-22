aa53761030031a352a1e6bfff4051fe56b2c0c96
Daniel Hiepler <d-git@coderdu.de>
2023-08-22 10:44:38 +0200

    reformatted file

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='document-dl',
-    version='0.2.1',
-    description='download documents from web portals',
+    name="document-dl",
+    version="0.2.1",
+    description="download documents from web portals",
     long_description=open("README.md").read(),
-    url='',
-    author='Daniel Hiepler',
-    author_email='d-docdl@coderdu.de',
-    license='unlicense',
-    keywords='scrape office documents bills',
-    py_modules=['docdl'],
+    url="",
+    author="Daniel Hiepler",
+    author_email="d-docdl@coderdu.de",
+    license="unlicense",
+    keywords="scrape office documents bills",
+    py_modules=["docdl"],
     install_requires=[
         'click',
         'click-plugins',
@@ -23,22 +23,22 @@ setup(
         'slugify',
         'watchdog'
     ],
-    packages=find_packages(exclude=['tests*']),
+    packages=find_packages(exclude=["tests*"]),
     entry_points={
-        'docdl_plugins': [
-            'amazon=docdl.plugins.amazon:amazon',
-            'believe=docdl.plugins.believe:believe',
-            'conrad=docdl.plugins.conrad:conrad',
-            'dkb=docdl.plugins.dkb:dkb',
-            'elster=docdl.plugins.elster:elster',
-            'handyvertrag=docdl.plugins.handyvertrag:handyvertrag',
-            'ing=docdl.plugins.ing:ing',
-            'o2=docdl.plugins.o2:o2',
-            'strato=docdl.plugins.strato:strato',
-            'vodafone=docdl.plugins.vodafone:vodafone'
+        "docdl_plugins": [
+            "amazon=docdl.plugins.amazon:amazon",
+            "believe=docdl.plugins.believe:believe",
+            "conrad=docdl.plugins.conrad:conrad",
+            "dkb=docdl.plugins.dkb:dkb",
+            "elster=docdl.plugins.elster:elster",
+            "handyvertrag=docdl.plugins.handyvertrag:handyvertrag",
+            "ing=docdl.plugins.ing:ing",
+            "o2=docdl.plugins.o2:o2",
+            "strato=docdl.plugins.strato:strato",
+            "vodafone=docdl.plugins.vodafone:vodafone",
         ],
-        'console_scripts': [
-            'document-dl=docdl.cli:documentdl',
-        ]
-    }
+        "console_scripts": [
+            "document-dl=docdl.cli:documentdl",
+        ],
+    },
 )
